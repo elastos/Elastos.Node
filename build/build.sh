0e6a86ef5c5122055884a22d449e61dc50d2e9d2
Li Bin <libin@elastos.org>
2021-06-02 17:46:59 +0800

    build script: add eth and arbiter; align source codes to latest versions; remove config templates files in skeleton

--- build/build.sh
+++ build/build.sh
@@ -6,12 +6,12 @@ setenv()
     local OS_VER="${OS_VER}_$(lsb_release -s -r 2>/dev/null)"
 
     if [ "$(uname -sm)" != "Linux x86_64" ]; then
-        echo "ERROR: this script requires Ubuntu 16.04 (x86_64)"
+        echo "ERROR: this script requires Ubuntu 16.04 (x86_64) or higher"
         exit
     fi
 
-    if [ "$OS_VER" != "Ubuntu_16.04" ]; then
-        echo "ERROR: this script requires Ubuntu 16.04 (x86_64)"
+    if [ "$OS_VER" \< "Ubuntu_16.04" ]; then
+        echo "ERROR: this script requires Ubuntu 16.04 (x86_64) or higher"
         exit
     fi
 
@@ -19,15 +19,15 @@ setenv()
     # Setup Go Lang
     #
     if [ ! -d $DEV_ROOT/build/go ]; then
-        local GO_TGZ_URL=https://dl.google.com/go/go1.10.8.linux-amd64.tar.gz
-        local GO_TGZ_SHA256=d8626fb6f9a3ab397d88c483b576be41fa81eefcec2fd18562c87626dbb3c39e
+        local GO_TGZ_URL=https://dl.google.com/go/go1.13.8.linux-amd64.tar.gz
+        local GO_TGZ_SHA256=0567734d558aef19112f2b2873caa0c600f1b4a5827930eb5a7f35235219e9d8
         local GO_TGZ=${GO_TGZ_URL##*/}
 
         cd $DEV_ROOT/build
         echo "Downloading $GO_TGZ..."
-        wget -O $GO_TGZ $GO_TGZ_URL
+        curl -O# $GO_TGZ_URL
         if [ "$?" != "0" ]; then
-            echo "ERROR: wget failed"
+            echo "ERROR: failed to download"
             exit
         fi
         if [ "$(shasum -a 256 $GO_TGZ | sed 's/ .*//')" != "$GO_TGZ_SHA256" ]; then
@@ -47,51 +47,12 @@ setenv()
     export GOPATH=$DEV_ROOT
 
     local GO_VERSION=$(go version 2>/dev/null)
-    if [ "$GO_VERSION" == "go version go1.10.8 linux/amd64" ]; then
+    if [ "$GO_VERSION" == "go version go1.13.8 linux/amd64" ]; then
         echo "INFO: found $GO_VERSION"
-    elif [ "$GO_VERSION" == "go version go1.10.8 darwin/amd64" ]; then
+    elif [ "$GO_VERSION" == "go version go1.13.8 darwin/amd64" ]; then
         echo "INFO: found $GO_VERSION"
     else
         echo "ERROR: no proper go"
-        exit
-    fi
-
-    #
-    # Setup Glide
-    #
-    if [ ! -d $DEV_ROOT/build/glide ]; then
-        local GLIDE_TGZ_URL=https://github.com/Masterminds/glide/releases/download/v0.13.1/glide-v0.13.1-linux-amd64.tar.gz
-        local GLIDE_TGZ_SHA256=c403933503ea40308ecfadcff581ff0dc3190c57958808bb9eed016f13f6f32c
-        local GLIDE_TGZ=${GLIDE_TGZ_URL##*/}
-
-        cd $DEV_ROOT/build
-        echo "Downloading $GLIDE_TGZ..."
-        wget -O $GLIDE_TGZ $GLIDE_TGZ_URL
-        if [ "$?" != "0" ]; then
-            echo "ERROR: wget failed"
-            exit
-        fi
-        if [ "$(shasum -a 256 $GLIDE_TGZ | sed 's/ .*//')" != "$GLIDE_TGZ_SHA256" ]; then
-            echo "ERROR: malformed package"
-            exit
-        fi
-        echo "Expanding $GLIDE_TGZ..."
-        mkdir -p glide
-        cd glide
-        tar xf ../$GLIDE_TGZ --strip=1
-        if [ "$?" != "0" ]; then
-            echo "ERROR: failed to extract"
-            exit
-        fi
-    fi
-
-    export PATH=$DEV_ROOT/build/glide:$PATH
-
-    local GLIDE_VERSION=$(glide -v 2>/dev/null)
-    if [ "$GLIDE_VERSION" == "glide version v0.13.1" ]; then
-        echo "INFO: found $GLIDE_VERSION"
-    else
-        echo "ERROR: no proper glide"
         exit
     fi
 
@@ -105,6 +66,19 @@ setenv()
             sudo apt-get install -yq $i
         fi
     done
+}
+
+commit_id()
+{
+    local BRANCH_TAG_NAME=$(git symbolic-ref -q --short HEAD)
+    if [ "$BRANCH_TAG_NAME" == "" ]; then
+        local BRANCH_TAG_NAME=$(git describe --tags --exact-match)
+    fi
+
+    local COMMIT_ID=$(git rev-parse --short HEAD)
+    local DIRTY=$(git diff --quiet || echo '-dirty')
+
+    echo ${BRANCH_TAG_NAME}-${COMMIT_ID}${DIRTY}
 }
 
 build_ela()
@@ -133,13 +107,7 @@ build_ela()
     git checkout $BRANCH_NAME
     git pull
     git status --ignored
-    local COMMIT_ID=$(git describe --abbrev=0 --dirty --always)
-    echo "ela: $COMMIT_ID" >commit.txt
-
-    echo "Installing dependencies..."
-    glide cc
-    glide update
-    glide install
+    echo "ela: $(commit_id)" >commit.txt
 
     echo "Compiling..."
     make
@@ -171,51 +139,7 @@ build_did()
     git checkout $BRANCH_NAME
     git pull
     git status --ignored
-    local COMMIT_ID=$(git describe --abbrev=0 --dirty --always)
-    echo "did: $COMMIT_ID" >commit.txt
-
-    echo "Installing dependencies..."
-    glide cc
-    glide update
-    glide install
-
-    echo "Compiling..."
-    make
-}
-
-build_token()
-{
-    if [ "$1" == "" ]; then
-        echo "Usage: ${FUNCNAME[0]} Branch|Commit"
-        echo "Build token"
-        return
-    fi
-
-    local BRANCH_NAME=$1
-
-    echo "Building token..."
-
-    if [ ! -d $DEV_ROOT/src/github.com/elastos/Elastos.ELA.SideChain.Token ]; then
-        mkdir -p $DEV_ROOT/src/github.com/elastos
-        cd $DEV_ROOT/src/github.com/elastos
-        git clone https://github.com/elastos/Elastos.ELA.SideChain.Token
-    fi
-
-    echo "Syncing..."
-    cd $DEV_ROOT/src/github.com/elastos/Elastos.ELA.SideChain.Token
-    git clean -fdx
-    git checkout master
-    git pull
-    git checkout $BRANCH_NAME
-    git pull
-    git status --ignored
-    local COMMIT_ID=$(git describe --abbrev=0 --dirty --always)
-    echo "token: $COMMIT_ID" >commit.txt
-
-    echo "Installing dependencies..."
-    glide cc
-    glide update
-    glide install
+    echo "did: $(commit_id)" >commit.txt
 
     echo "Compiling..."
     make
@@ -247,14 +171,80 @@ build_carrier()
     git checkout $BRANCH_NAME
     git pull
     git status --ignored
-    local COMMIT_ID=$(git describe --abbrev=0 --dirty --always)
-    echo "carrier: $COMMIT_ID" >commit.txt
+    echo "carrier: $(commit_id)" >commit.txt
 
     cd $DEV_ROOT/src/github.com/elastos/Elastos.NET.Carrier.Bootstrap/build
     mkdir -p linux
     cd linux
     cmake -DCMAKE_INSTALL_PREFIX=outputs ../..
     make install
+}
+
+build_eth()
+{
+    if [ "$1" == "" ]; then
+        echo "Usage: ${FUNCNAME[0]} Branch|Commit|Tag"
+        return
+    fi
+
+    local BRANCH_NAME=$1
+
+    echo "Building eth..."
+
+    rm -rf $DEV_ROOT/src/github.com/elastos/Elastos.ELA.SideChain.ETH
+    if [ ! -d $DEV_ROOT/src/github.com/elastos/Elastos.ELA.SideChain.ETH ]; then
+        mkdir -p $DEV_ROOT/src/github.com/elastos
+        cd $DEV_ROOT/src/github.com/elastos
+        git clone https://github.com/elastos/Elastos.ELA.SideChain.ETH.git
+    fi
+
+    echo "Syncing..."
+    cd $DEV_ROOT/src/github.com/elastos/Elastos.ELA.SideChain.ETH
+    git clean -fdx
+    git checkout .
+    git checkout master
+    git pull
+    git checkout $BRANCH_NAME
+    git pull
+    git status --ignored
+    echo "eth: $(commit_id)" >commit.txt
+
+    echo "Compiling..."
+    make all
+    #go build ./cmd/geth
+    #go build ./cmd/bootnode
+}
+
+build_arbiter()
+{
+    if [ "$1" == "" ]; then
+        echo "Usage: ${FUNCNAME[0]} Branch|Commit|Tag"
+        return
+    fi
+
+    local BRANCH_NAME=$1
+
+    echo "Building arbiter..."
+
+    if [ ! -d $DEV_ROOT/src/github.com/elastos/Elastos.ELA.Arbiter ]; then
+        mkdir -p $DEV_ROOT/src/github.com/elastos
+        cd $DEV_ROOT/src/github.com/elastos
+        git clone https://github.com/elastos/Elastos.ELA.Arbiter.git
+    fi
+
+    echo "Syncing..."
+    cd $DEV_ROOT/src/github.com/elastos/Elastos.ELA.Arbiter
+    git clean -fdx
+    git checkout .
+    git checkout master
+    git pull
+    git checkout $BRANCH_NAME
+    git pull
+    git status --ignored
+    echo "arbiter: $(commit_id)" >commit.txt
+
+    echo "Compiling..."
+    make
 }
 
 pack()
@@ -294,8 +284,16 @@ pack()
 
     echo "Copying skeleton..."
     cp -a $DEV_ROOT/build/skeleton $RELEASE_DIR/node
+    mkdir -p $RELEASE_DIR/node/carrier/
+    mkdir -p $RELEASE_DIR/node/ela/
+    mkdir -p $RELEASE_DIR/node/did/
+    mkdir -p $RELEASE_DIR/node/eth/
+    mkdir -p $RELEASE_DIR/node/arbiter/
 
     echo "Copying binaries..."
+    cp -v $DEV_ROOT/src/github.com/elastos/Elastos.NET.Carrier.Bootstrap/build/linux/src/ela-bootstrapd \
+        $RELEASE_DIR/node/carrier/
+
     cp -v $DEV_ROOT/src/github.com/elastos/Elastos.ELA/ela \
         $RELEASE_DIR/node/ela/
 
@@ -305,15 +303,11 @@ pack()
     cp -v $DEV_ROOT/src/github.com/elastos/Elastos.ELA.SideChain.ID/did \
         $RELEASE_DIR/node/did/
 
-    cp -v $DEV_ROOT/src/github.com/elastos/Elastos.ELA.SideChain.Token/token \
-        $RELEASE_DIR/node/token/
+    cp -v $DEV_ROOT/src/github.com/elastos/Elastos.ELA.SideChain.ETH/build/bin/geth \
+        $RELEASE_DIR/node/eth/
 
-    cp -v $DEV_ROOT/src/github.com/elastos/Elastos.NET.Carrier.Bootstrap/build/linux/src/ela-bootstrapd \
-        $RELEASE_DIR/node/carrier/
-
-    mkdir -p $RELEASE_DIR/node/carrier/var/lib/ela-bootstrapd
-    mkdir -p $RELEASE_DIR/node/carrier/var/lib/ela-bootstrapd/db
-    mkdir -p $RELEASE_DIR/node/carrier/var/run/ela-bootstrapd
+    cp -v $DEV_ROOT/src/github.com/elastos/Elastos.ELA.Arbiter/arbiter \
+        $RELEASE_DIR/node/arbiter/
 
     echo "Generating version.txt..."
     cd $RELEASE_DIR/node
@@ -321,10 +315,11 @@ pack()
 
     echo "Generating commit.txt..."
     cd $DEV_ROOT/src/github.com/elastos
-    cat Elastos.ELA/commit.txt                    >$RELEASE_DIR/node/commit.txt
+    cat Elastos.NET.Carrier.Bootstrap/commit.txt  >$RELEASE_DIR/node/commit.txt
+    cat Elastos.ELA/commit.txt                   >>$RELEASE_DIR/node/commit.txt
     cat Elastos.ELA.SideChain.ID/commit.txt      >>$RELEASE_DIR/node/commit.txt
-    cat Elastos.ELA.SideChain.Token/commit.txt   >>$RELEASE_DIR/node/commit.txt
-    cat Elastos.NET.Carrier.Bootstrap/commit.txt >>$RELEASE_DIR/node/commit.txt
+    cat Elastos.ELA.SideChain.ETH/commit.txt     >>$RELEASE_DIR/node/commit.txt
+    cat Elastos.ELA.Arbiter/commit.txt           >>$RELEASE_DIR/node/commit.txt
 
     echo "Generating checksum.txt..."
     cd $RELEASE_DIR/node
@@ -344,19 +339,21 @@ pack()
 
 usage()
 {
-    echo "Usage: $0 ELA_VER DID_VER TOKEN_VER CARRIER_VER"
+    echo "Usage: $0 CARRIER_VER ELA_VER DID_VER ETH_VER ARBITER_VER"
     echo "Build Elastos Supernode bundle package"
     echo
     echo "Arguments: branch or specific commit of the repositories:"
     echo
+    echo "  https://github.com/elastos/Elastos.NET.Carrier.Bootstrap"
     echo "  https://github.com/elastos/Elastos.ELA"
     echo "  https://github.com/elastos/Elastos.ELA.SideChain.ID"
-    echo "  https://github.com/elastos/Elastos.ELA.SideChain.Token"
-    echo "  https://github.com/elastos/Elastos.NET.Carrier.Bootstrap"
+    echo "  https://github.com/elastos/Elastos.ELA.SideChain.ETH"
+    echo "  https://github.com/elastos/Elastos.ELA.Arbiter"
     echo
     echo "Examples:"
-    echo "  $0 master master master master"
-    echo "  $0 v0.3.3 release_v0.1.2 release_v0.1.2 release-v5.2.3"
+    echo
+    echo "  $0 master master master master master"
+    echo "  $0 release-v6.0.1 v0.7.0 v0.2.1 v0.1.3.2 v0.2.1"
     echo
 }
 
@@ -365,16 +362,17 @@ usage()
 #
 DEV_ROOT=$(cd $(dirname $BASH_SOURCE)/..; pwd)
 
-if [ "$4" == "" ]; then
+if [ "$5" == "" ]; then
     usage
     exit
 fi
 
 setenv
 
-build_ela $1
-build_did $2
-build_token $3
-build_carrier $4
+build_carrier $1
+build_ela $2
+build_did $3
+build_eth $4
+build_arbiter $5
 
 pack
