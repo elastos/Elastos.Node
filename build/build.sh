855353e5f2ad6891c49e2b3bb7989817422cb9fd
Li Bin <libin@elastos.org>
2022-05-23 16:06:44 +0800

    Version bump in sample command

--- build/build.sh
+++ build/build.sh
@@ -398,7 +398,7 @@ usage()
     echo "Examples:"
     echo
     echo "  $0 master master master master master master master"
-    echo "  $0 release-v6.0.1 v0.7.0 v0.3.1 v0.1.3.2 v0.1.0 v0.2.3"
+    echo "  $0 release-v6.0.1 v0.8.3 v0.3.2 v0.1.4.4 v0.2.0 v0.3.1"
     echo
 }
 
